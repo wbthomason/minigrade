317c3480bbe077ac4ff1657d636e9b8c3f5f0a3f
Mister Abc <zl4ry@virginia.edu>
2014-02-22 02:40:17 -0500

    added functions for leaderboard updating

--- minigrade.py
+++ minigrade.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, Response, abort, session, stream_with_context, redirect
+from flask import Flask, render_template, request, jsonify, Response, abort, session, stream_with_context, redirect, g
 from ast import literal_eval
 import subprocess
 import re
@@ -7,11 +7,16 @@ import json
 import shutil
 import time
 import os
+import sqlite3
+
 
 minigrade = Flask(__name__)    
 # Put your own secret key here. You can't have mine!
 minigrade.secret_key = '!\xec\xa8\x88\xc9R\xf7i<wW\x9fzH\x81\xff\x11~aQn\x9f\xcf\x0b'
 urlmatch = re.compile('(?:git@|git://|https://)(?P<url>[\w@-]+\.[a-zA-Z]+[:/](?P<user>[a-zA-Z][a-zA-Z0-9-]+)/(?P<repo>.+))')
+
+PORT_NUMBER = 9080
+SERVER_IP = 'http://128.143.136.170'
 
 def process_repo(repo):
     result = urlmatch.match(repo)
@@ -46,6 +51,7 @@ def grade_stream(assignment, repo):
     build = None
     tests = []
     repo_name = "NotADirectory"
+    cwd = os.getcwd()
     try:
         with open("tests/{}.test".format(assignment)) as testfile:
             for idnum, testcase in enumerate(testfile):
@@ -160,7 +166,7 @@ def grade_stream(assignment, repo):
     finally:
         if os.path.isdir(repo_name):
             shutil.rmtree(repo_name)
-        os.chdir('/home/grader/minigrade')
+        os.chdir(cwd)
 
     yield "data: done\n\n"
 
@@ -184,7 +190,7 @@ def login():
         abort(400)
 
     # Send the assertion to Mozilla's verifier service.
-    data = {'assertion': request.form['assertion'], 'audience': 'http://128.143.136.170:9080'}
+    data = {'assertion': request.form['assertion'], 'audience': SERVER_IP + ':'+ str(PORT_NUMBER)}
     resp = requests.post('https://verifier.login.persona.org/verify', data=data, verify=True)
 
     # Did the verifier respond?
@@ -205,8 +211,121 @@ def login():
 def logout():
     session.pop('email', None)
     return redirect('/')
+
+
+# Server-side database methods
+##########
+
+
+@minigrade.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+        if error:
+            print("There was an error closing the database")
+
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect('database.db')
+    rv.row_factory = sqlite3.Row
+    return rv
+
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+def init_db():
+    """Creates the database tables."""
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+def query_db(query, args=(), one=False):
+    """Returns a query to the database as a list"""
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    get_db().commit()
+    return (rv[0] if rv else None) if one else rv
+
+# Leaderboard functions
+#################
+
+import random
+@minigrade.route('/leaderboard.html')
+def leaderboard():
+    update_leaderboard(5)
+    users = ['abc', 'zl4ry', 'asdf', 'jkl;', 'WIL']
+    update_top_runs(random.choice(users), random.random(), random.random())
+    with open("leaderboard.html") as sub_page:
+        return '\n'.join(sub_page.readlines())
+
+def update_top_runs(user, duration, response):
+    print(user, duration, response)
+    q = query_db("SELECT * FROM topruns WHERE username=?", [user], one=True)
+    print q
+    if q is None:
+	query_db("INSERT INTO topruns VALUES (?, ?, ?)", [user, str(duration), str(response)])
+    else:
+	query_db("UPDATE topruns SET duration=?, response=? WHERE username=?", [str(duration), str(response), user])
+
+def get_top_runs(num):
+    ''' Returns the top num runs in a list of 3xnum elements:
+	the first is best duration/response time,
+	the second is best duration, third is response time'''
+    runs = query_db("SELECT * FROM topruns")
+    data = [[],[],[]]
+    runs.sort(key=heuristic)
+    data[0] = runs[:num]
+    runs.sort(key=lambda x: float(x[1]))
+    data[1] = runs[:num]
+    runs.sort(key=lambda x: float(x[2]))
+    data[2] = runs[:num]
+    return data
+
+def heuristic(run):
+    '''returns a function of a weighing bewteen duration and response time'''
+    return float(run[1]) * float(run[2])
+
+def update_leaderboard(num):
+    '''Updates the leaderboard for webpages to see'''
+    head = "<h2>Leaderboard</h2>"
+    tbl_template=lambda x: '''
+<h3>%s</h3>
+<table id="leaderboard-dr" style='width:100%%%%;border-spacing:10px'>
+    <tr><th style="text-align:left">ID</th>
+	<th style="text-align:left">Duration Time</th>
+	<th style="text-align:left">Response Time</th>
+    </tr>
+    %%s
+</table>
+'''%x
+    titles = ["Best duration/response time", "Best duration", "Best Response Time"]
+
+    data = get_top_runs(num)
+    fin = ""
+    for i, title in enumerate(titles):
+	tmp = tbl_template(title)
+	row = ""
+	for tup in data[i]:
+	    # should be (username, duration, response time)
+	    row += "<tr><td>{}</td><td>{}</td><td>{}</td></tr>".format(*tup)
+	fin += tmp%row
+    open("leaderboard.html", 'w').write(fin)
     
+
 #Only run in chroot jail.
 if __name__ == '__main__':
-    minigrade.run(host='0.0.0.0', debug=False, threaded=True, port=9080)
+    minigrade.run(host='0.0.0.0', debug=False, threaded=True, port=PORT_NUMBER)
     #minigrade.run(debug=True, threaded=True, port=9080)
---
+++ schema.sql
@@ -0,0 +1,5 @@
+drop table if exists topruns;
+CREATE TABLE topruns ( 
+    username varchar(255) PRIMARY KEY, 
+    duration varchar(255), 
+    response varchar(255));
